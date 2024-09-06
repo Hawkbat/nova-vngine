2e732c2c17258119d6c2a24f6c21416a75390816
Joshua Thome <joshpthome@gmail.com>
2024-09-06 01:57:59 -0500

    GitHub Pages publishing script

---
+++ scripts/publish.js
@@ -0,0 +1,21 @@
+import { publish } from 'gh-pages'
+import * as fs from 'fs'
+
+console.log('Staging build...')
+
+fs.rmSync('dist', { recursive: true, force: true })
+fs.mkdirSync('dist', { recursive: true })
+fs.cpSync('resources', 'dist', { recursive: true })
+fs.cpSync('example-project', 'dist/project', { recursive: true })
+
+console.log('Publishing...')
+
+publish('dist', {
+    nojekyll: true,
+    cname: 'vngine.hawk.bar',
+    message: `Published ${new Date().toLocaleString()}`,
+}, err => {
+    console.error(err)
+})
+
+console.log('Published!')
