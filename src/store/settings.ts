8054bb28a8e3cc0d56d48082b272458d2b412305
Joshua Thome <joshpthome@gmail.com>
2024-09-07 03:30:57 -0500

    Fix missing settings defaults

--- src/store/settings.ts
+++ src/store/settings.ts
@@ -4,4 +4,11 @@ import { createSimpleStore } from '../utils/store'
 export const settingsStore = createSimpleStore<SettingsState>({
     developerMode: false,
     uiScale: 1,
+    menuAnimationEnabled: true,
+    scenePlayerSettings: {
+        musicVolume: 1,
+        soundVolume: 1,
+        uiVolume: 1,
+        textSpeed: 1,
+    },
 })
